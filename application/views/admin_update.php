9e51f04ca37fda8dfe50437c1365d55df9e96335
Dlin7 <david.lin.2012@gmail.com>
2014-03-27 16:12:27 -0400

    Update admin_update.php

--- application/views/admin_update.php
+++ application/views/admin_update.php
@@ -1,7 +1,7 @@
 <h1><?php echo $Title; ?> | Admin</h1>
 <?php foreach($post as $p): ?>
   <?php echo form_open('admin/update_post/' . $p['ID']) ?>
-    <label>Title123</label>
+    <label>Title</label>
     <input type="text" name="Title" value="<?php echo $p['Title']; ?>">
     <label>Content</label>
     <textarea name="Content"><?php echo $p['Content']; ?></textarea>
